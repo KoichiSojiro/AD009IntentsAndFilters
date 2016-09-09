01f2c5649d0862a76580150467bddfe91602de95
Koichi <koichisojiro@live.com>
2016-09-09 15:38:11 +0700

    complete

---
+++ app/src/main/java/com/example/trannh08/ad009intentsandfilters/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.trannh08.ad009intentsandfilters;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button_startBrowser, button_startPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_startBrowser = (Button) findViewById(R.id.button_startBrowser);
+        button_startBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(intent);
+            }
+        });
+
+        button_startPhone = (Button) findViewById(R.id.button_startPhone);
+        button_startPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:84937432001"));
+                startActivity(intent);
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+}
